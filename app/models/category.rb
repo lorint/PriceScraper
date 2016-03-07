20cccaa2eb05e73ecd0989ffda9931d5c7418c44
Lorin Thwaits <lorin@Lorins-MacBook-Air.local>
2016-03-07 16:16:52 +0000

    Small bug in parent method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :parent, class_name: "Category", foreign_key: :parent_id, inverse_of: :children
-  has_many :children, class_name: "Category", foreign_key: :parent_id, inverse_of: parent
+  has_many :children, class_name: "Category", foreign_key: :parent_id, inverse_of: :parent
 end
